447996ff2b637833c6e7feb00c02937ab635935e
⏤͟͟͞͞𝙃𝙔𝙋𝙀𝙍 𝙊𝙋 ™ 〆⁮⁮⁮ 🇮🇳 𖤍 <priyankabillo49@gmail.com>
2025-03-31 17:43:03 +0530

    Add files via upload

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,53 +1,38 @@
+import json
+import os
 
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-
-import config
 from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
 from VIPMUSIC.core.userbot import Userbot
+from VIPMUSIC.core.youtube import vipboy
 from VIPMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
-# Pyrogram Client
+dirr()
 
-app = VIPMUSIC(
-    "VIPMUSIC",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    sleep_threshold=240,
-    max_concurrent_transmissions=5,
-    workers=50,
-)
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+vipboy()
+
+app = VIPBot()
 
 userbot = Userbot()
 
-# Directories
-dirr()
+from .platforms import *
 
-# Check Git Updates
-git()
-
-# Initialize Memory DB
-dbb()
-
-# Heroku APP
-heroku()
-
-# Load Sudo Users from DB
-sudo()
-
-
-from .platforms import PlaTForms
-
-Platform = PlaTForms()
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
 HELPABLE = {}
--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -7,29 +7,35 @@
 #
 # All rights reserved.
 import asyncio
-import os
+import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
 from VIPMUSIC import HELPABLE, LOGGER, app, userbot
 from VIPMUSIC.core.call import VIP
+from VIPMUSIC.plugins import ALL_MODULES
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
-
-logger = LOGGER("YukkiMusic")
-loop = asyncio.get_event_loop()
 
 
 async def init():
-    if len(config.STRING_SESSIONS) == 0:
-        logger.error("No Assistant Clients Vars Defined!.. Exiting Process.")
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("VIPMUSIC").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
         return
     if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
-        logger.warning(
+        LOGGER("VIPMUSIC").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
+
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -39,60 +45,24 @@ async def init():
             BANNED_USERS.add(user_id)
     except Exception:
         pass
+
     await app.start()
-    for mod in app.load_plugins_from("VIPMUSIC/plugins"):
-        if mod and hasattr(mod, "__MODULE__") and mod.__MODULE__:
-            if hasattr(mod, "__HELP__") and mod.__HELP__:
-                HELPABLE[mod.__MODULE__.lower()] = mod
 
-    if config.EXTRA_PLUGINS:
-        if os.path.exists("xtraplugins"):
-            result = await app.run_shell_command(["git", "-C", "xtraplugins", "pull"])
-            if result["returncode"] != 0:
-                logger.error(
-                    f"Error pulling updates for extra plugins: {result['stderr']}"
-                )
-                exit()
-        else:
-            result = await app.run_shell_command(
-                ["git", "clone", config.EXTRA_PLUGINS_REPO, "xtraplugins"]
-            )
-            if result["returncode"] != 0:
-                logger.error(f"Error cloning extra plugins: {result['stderr']}")
-                exit()
+    for all_module in ALL_MODULES:
+        imported_module = importlib.import_module(all_module)
 
-        req = os.path.join("xtraplugins", "requirements.txt")
-        if os.path.exists(req):
-            result = await app.run_shell_command(["pip", "install", "-r", req])
-            if result["returncode"] != 0:
-                logger.error(f"Error installing requirements: {result['stderr']}")
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
 
-        for mod in app.load_plugins_from("xtraplugins"):
-            if mod and hasattr(mod, "__MODULE__") and mod.__MODULE__:
-                if hasattr(mod, "__HELP__") and mod.__HELP__:
-                    HELPABLE[mod.__MODULE__.lower()] = mod
-
-    LOGGER("YukkiMusic.plugins").info("Successfully Imported All Modules ")
     await userbot.start()
-    await Yukki.start()
-    LOGGER("VIPMUSIC").info("Assistant Started Sucessfully")
-    try:
-        await Yukki.stream_call(
-            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
-        )
-    except NoActiveGroupCall:
-        LOGGER("VIPMUSIC").error(
-            "Please ensure the voice call in your log group is active."
-        )
-        exit()
-
-    await Yukki.decorators()
-    LOGGER("VIPMUSIC").info("VIPMUSIC Started Successfully")
+    await VIP.start()
+    await VIP.decorators()
+    LOGGER("VIPMUSIC").info("VIPMUSIC STARTED SUCCESSFULLY 🕊️")
     await idle()
-    await app.stop()
-    await userbot.stop()
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(init())
-    LOGGER("VIPMUSIC").info("Stopping YukkiMusic! GoodBye")
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    LOGGER("VIPMUSIC").info("Stopping VIPMUSIC! GoodBye")
